6e248e94a183d34c88f5ee4aeb0ffe318db5d379
Nahum DevOps <nahumm01@outlook.com>
2024-10-03 20:32:50 -0300

    Refatorando código com melhor código e legibilidade

--- src/CalculaPorcentagem.java
+++ src/CalculaPorcentagem.java
@@ -6,23 +6,18 @@ public class CalculaPorcentagem {
 		System.out.println("Digite seu salário: ");
 		double salario = sc.nextDouble();
 
-		Scanner sc2 = new Scanner(System.in);
-		System.out.println("Digite sua faixa do Imposto de Renda: (1/2/3)");
-		int faixa = sc2.nextInt();
-
-		double primeiraFaixaIR = 0.05;
-		double segundaFaixaIR = 0.15;
-		double terceiraFaixaIR = 0.30;
-
-		if (faixa == 1) {
-			System.out.println("Está é a quantidade descontada na sua faixa: " + salario * primeiraFaixaIR);
-			System.out.println("Seu salário líquido: " + (salario - (salario * primeiraFaixaIR)));
-		}else if (faixa == 2) {
-			System.out.println("Esta é a quantidade descontada na sua faixa: " + salario * segundaFaixaIR);
-			System.out.println("Seu salário líquido: " + (salario - (salario * segundaFaixaIR)));
-		} else if (faixa == 3) {
-			System.out.println("Esta é a quantidade descontada na sua faixa: " + salario * terceiraFaixaIR);
-			System.out.println("Seu salário líquido " + (salario - (salario * terceiraFaixaIR)));
+		if (salario <= 1412.00) {
+			double primeiraFaixaIR = 0.05;
+			System.out.println("Está é a quantidade descontada na sua faixa: " + salario * primeiraFaixaIR + " Corresponde a " + primeiraFaixaIR*100 + "%");
+			System.out.println("Seu salário líquido R$: " + (salario - (salario * primeiraFaixaIR)));
+		}else if (salario >= 1412.01 && salario <= 2428.00) {
+			double segundaFaixaIR = 0.15;
+			System.out.println("Esta é a quantidade descontada na sua faixa: " + salario * segundaFaixaIR + " Corresponde a " + segundaFaixaIR*100 + "%");
+			System.out.println("Seu salário líquido R$: " + (salario - (salario * segundaFaixaIR)));
+		} else if (salario >= 2428.01 && salario <= 4856.00) {
+			double terceiraFaixaIR = 0.30;
+			System.out.println("Esta é a quantidade descontada na sua faixa: " + salario * terceiraFaixaIR + " Corresponde a " + terceiraFaixaIR*100 + "%");
+			System.out.println("Seu salário líquido R$:" + (salario - (salario * terceiraFaixaIR)));
 		}else {
 			System.out.println("Digite a faixa correta: 1/2/3!");
 		}
